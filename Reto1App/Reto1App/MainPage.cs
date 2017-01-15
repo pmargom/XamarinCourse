5d4727f0b46b01805dc7dcb2857d32ba2db84f4e
Pedro Martín <pmargom@gmail.com>
2017-01-15 22:25:27 +0000

    Reto 1 -> Done

---
+++ Reto1App/Reto1App/MainPage.cs
@@ -0,0 +1,14 @@
+﻿using Xamarin.Forms;
+
+namespace Reto1App
+{
+	public partial class MainPage : ContentPage
+	{
+		int numClicks = 1; 		public MainPage() 		{ 			InitializeComponent();
+
+			btnPulsame.Clicked += (sender, e) => 
+			{
+				lblMensaje.Text = string.Format("Botó pulsado {0} veces", numClicks++);
+			};  		}
+	}
+}
